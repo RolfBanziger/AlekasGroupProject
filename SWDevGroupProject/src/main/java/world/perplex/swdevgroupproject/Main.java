388f8774aea1230d4f76a84bf4739597a5438dfc
Rolf Bänziger <rolf@perplex.world>
2022-03-24 14:55:48 +0000

    Change hello to byebye

--- SWDevGroupProject/src/main/java/world/perplex/swdevgroupproject/Main.java
+++ SWDevGroupProject/src/main/java/world/perplex/swdevgroupproject/Main.java
@@ -11,6 +11,6 @@ package world.perplex.swdevgroupproject;
  */
 public class Main {
     public static void main (String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Bybebye World");
     }
 }
